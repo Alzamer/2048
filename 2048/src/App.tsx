cdfd1dc96281a5e4a7af04ea1c801708b70d55e6
alzamer <mic.szendzielorz@gmail.com>
2023-12-01 22:12:40 +0100

    Added description

--- 2048/src/App.tsx
+++ 2048/src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <h1>Hello, World!</h1>
+      <p>Hello! This is a clone of a popular 2048 game, which I made with TypeScript and React. You can view the source code there.</p>
     </>
   )
 }
