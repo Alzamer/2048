c0e223d766378e375b0a832e2d75c0d1995ec16b
alzamer <mic.szendzielorz@gmail.com>
2023-12-02 20:29:31 +0100

    Basic config

--- 2048/src/App.tsx
+++ 2048/src/App.tsx
@@ -1,11 +1,12 @@
+import Grid from './Grid'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>Hello, World!</h1>
-      <p>Hello! This is a clone of a popular 2048 game, which I made with TypeScript and React. You can view the source code there.</p>
-    </>
+    <div className="container">
+      <Grid/>
+      <p>Hello! This is a clone of a popular 2048 game, which I made with TypeScript and React. You can view the source code <a href="https://github.com/Alzamer/2048" target="blank">there</a></p>
+    </div>
   )
 }
 
---
+++ 2048/src/Grid/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+function Grid() {
+  return (
+    <div className={styles.container}>
+      <p>GRID</p>
+    </div>
+  )
+}
+  
+export default Grid
