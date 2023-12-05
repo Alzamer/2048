3a85f94105ee81d8b0e9352eed19bb10f2e59aae
alzamer <mic.szendzielorz@gmail.com>
2023-12-05 11:28:09 +0100

    Added description

--- 2048/src/App.tsx
+++ 2048/src/App.tsx
@@ -1,11 +1,12 @@
 import Grid from './Grid'
+import Description from './Description'
 import './App.css'
 
 function App() {
   return (
     <div className="container">
       <Grid/>
-      <p>Hello! This is a clone of a popular 2048 game, which I made with TypeScript and React. You can view the source code <a href="https://github.com/Alzamer/2048" target="blank">there</a></p>
+      <Description/>
     </div>
   )
 }
