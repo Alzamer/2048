6a290a430513b69be186d473855a87233e9dc999
alzamer <mic.szendzielorz@gmail.com>
2023-12-07 16:21:14 +0100

    Polished description component

--- 2048/src/Description/index.tsx
+++ 2048/src/Description/index.tsx
@@ -2,9 +2,11 @@ import styles from './style.module.css'
 
 function Description() {
   return (
-    <p className={styles.text}>
-      Hello! This is a clone of a popular 2048 game, which I made with TypeScript and React. You can view the source code <a href="https://github.com/Alzamer/2048">there</a>.
-    </p>
+    <div className={styles.container}>
+      <p className={styles.text}>
+        Hello! This is a clone of a popular 2048 game, which I made with TypeScript and React. You can view the source code <a href="https://github.com/Alzamer/2048">there</a>.
+      </p>
+    </div>
   )
 }
 
