7bd888560f040b2ec21522f953768a8dc2104e7b
alzamer <mic.szendzielorz@gmail.com>
2023-12-08 17:35:21 +0100

    Added new game button

--- 2048/src/Description/index.tsx
+++ 2048/src/Description/index.tsx
@@ -2,12 +2,15 @@ import styles from './style.module.css'
 
 function Description() {
   return (
-    <div className={styles.container}>
+    <>
+      <div className={styles.container}>
       <p className={styles.text}>
-        Hello! This is a clone of a popular 2048 game, which I made with TypeScript and React. You can view the source code <a href="https://github.com/Alzamer/2048">there</a>.
+        Hello! This is a clone of a popular 2048 game, which I made with TypeScript and React. You can view the source code <a href="https://github.com/Alzamer/2048" target="_blank">there</a>.
       </p>
-    </div>
-  )
-}
+      </div>
+      <p className={styles.text}>Score: </p>
+      <button>New Game</button>
+    </>
+)}
 
 export default Description
