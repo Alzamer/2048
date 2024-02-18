9b11d696cb3443c0fabf90f889ff238b7348140c
alzamer <mic.szendzielorz@gmail.com>
2024-02-18 15:18:26 +0100

    Added event listeners

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Grid from './Grid'
 import Description from './Description'
 import './App.css'
 import { useState } from 'react'
+import EventListeners from './EventListeners/EventListeners'
 
 function App() {
   const [gridState, setGridState] = useState<Array<Array<number>>>([
@@ -13,8 +14,10 @@ function App() {
 
   return (
     <div className="container">
-      <Grid grid={gridState} setGridState={setGridState}/>
-      <Description setGridState={setGridState}/>
+      <EventListeners>
+        <Grid grid={gridState} setGridState={setGridState}/>
+        <Description setGridState={setGridState}/>
+      </EventListeners>
     </div>
   )
 }
--- src/EventListeners/EventListeners.tsx
+++ src/EventListeners/EventListeners.tsx
@@ -1,10 +1,27 @@
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
 function EventListeners({ children } : { children: any}) {
   useEffect(() => {
-    
+    const handleKeyPress = (event: any) => {
+      if (event.key === 'ArrowLeft' || event.key === 'a') {
+        console.log('Kliknięto strzałkę w lewo lub klawisz a');
+      } else if (event.key === 'ArrowRight' || event.key === 'd') {
+        console.log('Kliknięto strzałkę w prawo lub klawisz d');
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
   }, [])
-  return null;
+
+  return <>
+    {
+      children
+    }
+  </>;
 }
     
 export default EventListeners
