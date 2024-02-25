ee3b3508f4d7b3730c1aa0a4aa00cb12c7738ba5
alzamer <mic.szendzielorz@gmail.com>
2024-02-25 14:35:04 +0100

    Added new styling to the grid

--- src/Tile/index.tsx
+++ src/Tile/index.tsx
@@ -1,19 +1,21 @@
 import styles from './style.module.css'
+const colors = {
+  '2': '#EEE4DA',
+  '4': '#EDE0C8',
+  '8': '#F2B179',
+  '16': '#F59563',
+  '32': '#F67C5F',
+  '64': '#F65E3B',
+  '128': '#EDCF72',
+  '256': '#EDCC61',
+  '512': '#EDC850',
+  '1024': '#EDC53F',
+  '2048': '#EDC22E',
+}
 
-function Tile({ children } : { children : number}) {
-  const colors = {
-    '2': '#776E65',
-    '4': '#776E65',
-    '8': '#F2B179',
-    '16': 'F59563',
-    '32': '#F67C5F',
-    '64': '#F65E3B',
-    '128': '#EDCF72',
-    '256': '#EDCC61',
-    '512': '#EDC850',
-    '1024': '#EDC53F',
-    '2028': '#EDC22E',
-  }
+type typ = keyof typeof colors;
+
+function Tile({ children } : { children : typ}) {
 
   return (
     <div className={styles.container} style={{ background: colors[`${children}`] }}>
