cb061753c573f88757c5133ac85e8ac0e43baf7f
alzamer <mic.szendzielorz@gmail.com>
2024-02-28 15:02:23 +0100

    Added useMoveLeft

--- src/util/moveLeft.ts
+++ src/util/moveLeft.ts
@@ -2,20 +2,16 @@ import addRandomPoint from "./addRandomPoint";
 
 export default function useMoveLeft(state : any) {
   let changed = false;
-
   for (let i = 0; i < 4; i++) {
-    for (let j = 3; j >= 1; j--) {
-      if (state.gridState[i][j] === 0)
+    for (let j = 1; j < 4; j++) {
+      if(state.gridState[i][j] === 0)
         continue;
-      else if (state.gridState[i][j - 1] === 0) {
-        state.gridState[i][j - 1] = state.gridState[i][j];
-        state.gridState[i][j] = 0;
+      let x = j;
+      while(x > 0 && state.gridState[i][x - 1] === 0){
+        state.gridState[i][x - 1] = state.gridState[i][x];
+        state.gridState[i][x] = 0;
         changed = true;
-      }
-      else if (state.gridState[i][j - 1] === state.gridState[i][j]) {
-        state.setScore(state.score + 2 * state.gridState[i][j]);
-        state.gridState[i][j - 1] *= 2;
-        state.gridState[i][j] = 0;
+        x--;
       }
     }
   }
