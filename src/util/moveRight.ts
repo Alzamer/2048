ff99aa67782faa267066c966fbb8a984721c0969
alzamer <mic.szendzielorz@gmail.com>
2024-02-28 15:02:50 +0100

    Added useMoveRight

--- src/util/moveRight.ts
+++ src/util/moveRight.ts
@@ -2,20 +2,16 @@ import addRandomPoint from "./addRandomPoint";
 
 export default function useMoveRight(state : any) {
   let changed = false;
-  
   for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 3; j++) {
-      if (state.gridState[i][j] === 0)
+    for (let j = 2; j >= 0; j--) {
+      if(state.gridState[i][j] === 0)
         continue;
-      else if (state.gridState[i][j + 1] === 0) {
-        state.gridState[i][j + 1] = state.gridState[i][j];
-        state.gridState[i][j] = 0;
+      let x = j;
+      while(x < 3 && state.gridState[i][x + 1] === 0){
+        state.gridState[i][x + 1] = state.gridState[i][x];
+        state.gridState[i][x] = 0;
         changed = true;
-      }
-      else if (state.gridState[i][j + 1] === state.gridState[i][j]) {
-        state.setScore(state.score + 2 * state.gridState[i][j]);
-        state.gridState[i][j + 1] *= 2;
-        state.gridState[i][j] = 0;
+        x++;
       }
     }
   }
